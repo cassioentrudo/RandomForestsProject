7c591467e1e4ce1781fdffd42b6c96307caf6d9f
RodolfoA <rodolfo@enseg.com>
2019-05-15 17:59:17 -0300

    k_fold as a function and working for any number

--- k_folds.py
+++ k_folds.py
@@ -1,20 +1,31 @@
 from DadosTreinamento import table
+import training
 import pandas as pd
-import numpy as np
 
-n=2
-answer="Joga"
 
 #%%
 
-foldSize = foldSize=len(table)/n
-#shufTable = table.sample(frac=1).reset_index(drop=True)
-orderedTable = table.sort_values(answer)
-stratified = pd.DataFrame();
-for x in range(n):
-    for y in range (0, len(orderedTable), n):
-        pdobject = pd.DataFrame(orderedTable.iloc[y+x])
-        pdobject = pdobject.transpose()
-        #print pdobject
-        stratified = stratified.append(pdobject)
-folds = [stratified[x:x+foldSize] for x in range (0, len(stratified), foldSize)]
+def k_folding(kTable, kN, kTarget):
+    
+    totalSize = len(kTable)
+    foldSize = totalSize//kN
+    if (totalSize%kN>0):
+        foldSize = foldSize + 1
+    #shufTable = table.sample(frac=1).reset_index(drop=True)
+    orderedTable = kTable.sort_values(kTarget)
+    stratified = pd.DataFrame();
+    for x in range(kN):
+        for y in range (0, totalSize, kN):
+            #print(y+x)
+            if (y+x<totalSize):
+                pdobject = pd.DataFrame(orderedTable.iloc[y+x])
+                pdobject = pdobject.transpose()
+                stratified = stratified.append(pdobject)
+    folds = [stratified[x:x+foldSize] for x in range (0, totalSize, foldSize)]
+    return folds
+
+#%%
+
+n=2
+target=training.GetTargetFeature()
+fold = k_folding(table, n, target)
