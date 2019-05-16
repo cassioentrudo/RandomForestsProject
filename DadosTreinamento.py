3a4a1040f7b86d68f039e989b95ae0dc434b084c
rsabedra <h2sabedra@hotmail.com>
2019-05-15 23:25:54 -0300

    corrigido bug dataframe table drop

--- DadosTreinamento.py
+++ DadosTreinamento.py
@@ -4,7 +4,6 @@ import numpy as np
 
 def DataRead(str1):
     dataTable = pd.read_csv("%s" % str1, sep="\s*\;",  engine='python')
-    dataTable.drop(dataTable.index[[0]], inplace=True)
     return dataTable
 
 
