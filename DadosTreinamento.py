ec50d226d0caaaee50a4ef267b6ddfed7f97ee9f
RodolfoA <rodolfo@enseg.com>
2019-05-16 18:05:58 -0300

    arrumando o dadosTreinamento

--- DadosTreinamento.py
+++ DadosTreinamento.py
@@ -2,8 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-tablePath = "dadosBenchmark_validacaoAlgoritmoAD.csv"
-isNumeric = False
+#tablePath = "dadosBenchmark_validacaoAlgoritmoAD.csv"
+isNumeric = True
+resultIsNumeric = False
+#tablePath = "wdbc.data"
+#tablePath = "wine.data"
+tablePath = "ionosphere.data"
 
 def DataRead(str1):
     if(isNumeric==True):
@@ -12,9 +16,24 @@ def DataRead(str1):
         dataTable = pd.read_csv("%s" % str1, sep="\s*\;",  engine='python')
     return dataTable
 
+
 table = DataRead(tablePath)
+names = {}
+for x in range(len(table.columns)):
+    number = x+65
+    if (number>90):
+        number = number+6
+    names[x]=chr(number)
+table = table.rename(columns=names)
 
+if(tablePath == "wdbc.data"):
+    table = table.drop(columns="A")
+    aux = table["B"]
+    table = table.drop(columns="B")
+    table["result"]=aux
 
-
-
-#tablePath = "breast-cancer-wisconsin.data"
+if (tablePath == "wine.data"):
+    aux = table["A"]
+    table = table.drop(columns="A")
+    table["result"]=aux
+    
