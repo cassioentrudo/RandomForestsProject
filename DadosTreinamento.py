f0df981ca6cc7007bbe40944445fd5f04b606d70
rsabedra <h2sabedra@hotmail.com>
2019-05-16 00:11:42 -0300

    começando a modificação para atributos numéricos

--- DadosTreinamento.py
+++ DadosTreinamento.py
@@ -4,10 +4,11 @@ import numpy as np
 
 def DataRead(str1):
     dataTable = pd.read_csv("%s" % str1, sep="\s*\;",  engine='python')
+    #dataTable = pd.read_csv("%s" % str1,header=None, sep="\s*\,",  engine='python') #PARA ATRIBUTOS NUMÉRICOS
     return dataTable
 
-
-tablePath = "dadosBenchmark_validacaoAlgoritmoAD.csv"
+tablePath = "dadosBenchmark_validacaoAlgoritmoAD"
+#tablePath = "breast-cancer-wisconsin.data"
 
 
 
