17d7ec42c6fce7e4577d1a7ed879e93372e19b9e
cassio.entrudo <cassio.entrudo@gmail.com>
2019-05-16 11:50:46 -0300

    corrigido .csv no final do dome do arquivo

--- DadosTreinamento.py
+++ DadosTreinamento.py
@@ -7,7 +7,7 @@ def DataRead(str1):
     #dataTable = pd.read_csv("%s" % str1,header=None, sep="\s*\,",  engine='python') #PARA ATRIBUTOS NUMÉRICOS
     return dataTable
 
-tablePath = "dadosBenchmark_validacaoAlgoritmoAD"
+tablePath = "dadosBenchmark_validacaoAlgoritmoAD.csv"
 #tablePath = "breast-cancer-wisconsin.data"
 
 
