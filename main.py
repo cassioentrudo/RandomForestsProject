15672901ddae9ab2a3715eac2e86a3ea7db11fcc
RodolfoA <rodolfo@enseg.com>
2019-05-17 17:46:27 -0300

    Counter for VP, VN, FP, FN

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #MAIN
-from DadosTreinamento import table
+from DadosTreinamento import table, tablePath
 import k_folds
 import training
 import ensemble
 import votation
 
-numFolds=10
+numFolds=4
 nTrees=1
 
 
@@ -21,7 +21,7 @@ def main():
         trainingFolds.pop(x)
         forrest = ensemble.afforestation(folds, nTrees)
         mostVoted=votation.categoricVotation(forrest, testFold, training.GetTargetFeature())
-        originalResults.append(testFold[training.GetTargetFeature()])
+        originalResults.append(testFold[training.GetTargetFeature()].as_matrix())
         mostVotes.append(mostVoted)
         print(testFold)
         print(mostVoted)
@@ -29,6 +29,39 @@ def main():
 
     print("Original: ", originalResults)    
     print("Most Voted: ", mostVotes)
+    VP=0
+    VN=0
+    FP=0
+    FN=0
+    if (tablePath == "wdbc.data"):
+        for x in range(len(mostVotes)):
+            for y in range(len(mostVotes[x])):
+                if (mostVotes[x][y]=="=B"):
+                    if(originalResults[x][y]=="B"):
+                        VP=VP+1
+                    else:
+                        FP=FP+1
+                else:
+                    if(originalResults[x][y]=="B"):
+                        FN=FN+1
+                    else:
+                        VN=VN+1
+        print("VP: ", VP, " VN: ", VN, " FP: ", FP, " FN: ", FN)
     
+    if (tablePath == "ionosphere.data"):
+        for x in range(len(mostVotes)):
+            for y in range(len(mostVotes[x])):
+                if (mostVotes[x][y]=="=g"):
+                    if(originalResults[x][y]=="g"):
+                        VP=VP+1
+                    else:
+                        FP=FP+1
+                else:
+                    if(originalResults[x][y]=="g"):
+                        FN=FN+1
+                    else:
+                        VN=VN+1
+        print("VP: ", VP, " VN: ", VN, " FP: ", FP, " FN: ", FN)
+        
 if __name__ == "__main__":
     main()
