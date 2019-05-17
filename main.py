1385764be2db51742c662428518f67e4d9e6a76a
RodolfoA <rodolfo@enseg.com>
2019-05-17 15:42:01 -0300

    Facilitação da leitura dos dados

--- main.py
+++ main.py
@@ -6,23 +6,29 @@ import ensemble
 import votation
 
 numFolds=10
-nTrees=3
+nTrees=1
 
 
 #%%
 
 def main():
     folds = k_folds.k_folding(table, numFolds, training.GetTargetFeature())
+    originalResults = []
+    mostVotes = []
     for x in range(len(folds)):
         trainingFolds=folds
         testFold=folds[x]
         trainingFolds.pop(x)
         forrest = ensemble.afforestation(folds, nTrees)
         mostVoted=votation.categoricVotation(forrest, testFold, training.GetTargetFeature())
+        originalResults.append(testFold[training.GetTargetFeature()])
+        mostVotes.append(mostVoted)
         print(testFold)
         print(mostVoted)
         print("####################################   NOVA FOLD   ######################################################")
-    
+
+    print("Original: ", originalResults)    
+    print("Most Voted: ", mostVotes)
     
 if __name__ == "__main__":
     main()
